c31ee39b114a17b03977f8818099d13a7017918d
Stefan Antoni <stefan.antoni@deutschebahn.com>
2020-02-11 17:02:36 +0100

    Add latest

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,10 +6,10 @@ import dracula.draw
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
-        'horizontal': 8
+        'horizontal': 6
     },
     'font': {
-        'family': 'SF Mono, Menlo, Terminus, Monaco, Monospace',
+        'family': 'Hack Nerd Font Regular, Menlo, Terminus, Monaco, Monospace',
         'size': 10
     }
 })
@@ -17,25 +17,27 @@ dracula.draw.blood(c, {
 c.auto_save.session = True
 c.content.host_blocking.enabled = True
 c.content.headers.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+c.content.user_stylesheets = "/Users/stefanantoni/.qutebrowser/style.css"
 c.completion.use_best_match = True
 c.tabs.position = "left"
 c.window.hide_decoration = False
 c.url.searchengines = { "DEFAULT": "https://www.google.de/search?q={}" }
 c.url.start_pages = "https://news.ycombinator.com"
-c.fonts.monospace = 'SF Mono "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal"'
+c.fonts.monospace = 'Hack Nerd Font Regular, "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal"'
 c.editor.command = ["code", "{file}"]
-c.fonts.completion.category = "bold 14pt SF Mono"
-c.fonts.completion.entry = "13pt SF Mono"
-c.fonts.debug_console = "13pt SF Mono"
-c.fonts.downloads = "13pt SF Mono"
-c.fonts.hints = "bold 13pt SF Mono"
-c.fonts.keyhint = "13pt SF Mono"
-c.fonts.messages.error = "13pt SF Mono"
-c.fonts.messages.info = "13pt SF Mono"
-c.fonts.messages.warning = "13pt SF Mono"
-c.fonts.prompts = "13pt SF Mono"
-c.fonts.statusbar = "13pt SF Mono"
-c.fonts.tabs = "13pt SF Mono"
+c.fonts.completion.category = "Hack Nerd Font Regular"
+c.fonts.completion.entry = "Hack Nerd Font Regular"
+c.fonts.completion.entry = "Hack Nerd Font Regular"
+c.fonts.debug_console = "Hack Nerd Font Regular"
+c.fonts.downloads = "Hack Nerd Font Regular"
+c.fonts.hints = "Hack Nerd Font Regular"
+c.fonts.keyhint = "Hack Nerd Font Regular"
+c.fonts.messages.error = "Hack Nerd Font Regular"
+c.fonts.messages.info = "Hack Nerd Font Regular"
+c.fonts.messages.warning = "Hack Nerd Font Regular"
+c.fonts.prompts = "Hack Nerd Font Regular"
+c.fonts.statusbar = "Hack Nerd Font Regular"
+c.fonts.tabs = "Hack Nerd Font Regular"
 
 c.input.insert_mode.auto_enter = True
 c.input.insert_mode.auto_leave = False
@@ -45,3 +47,9 @@ c.tabs.background = True
 
 # Experimental: Allow passthru Escape to websites
 config.bind('<Escape>', 'fake-key <Escape>')
+
+# xt for toggling tabs
+config.bind('xt', 'onfig-cycle tabs.show always switching')
+
+# Load autoconfig file if existing
+config.load_autoconfig()
